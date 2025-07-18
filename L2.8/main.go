13305d71b382834796ea8d6d2ad18de9a1ff1aa2
Vladimirmoscow84 <ccr1@yandex.ru>
2025-07-18 23:15:04 +0300

    L2.8 nontest

--- L2.8/main.go
+++ L2.8/main.go
@@ -11,8 +11,33 @@
 
 Код должен проходить проверки (vet и golint), т.е. быть написан идиоматически корректно.*/
 
+//воспользуемся документацией по адресу https://pkg.go.dev/github.com/beevik/ntp#section-readme
+
 package main
 
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
 func main() {
+	currentTime, err := getTime()
+	if err != nil {
+		fmt.Println(os.Stderr, err)
+		os.Exit(1)
+	}
+	fmt.Printf("Точное текущее время: %v", currentTime)
+
+}
+
+func getTime() (time.Time, error) {
+	t, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	if err != nil {
+		return time.Time{}, err
+	}
+	return t, nil
 
 }
